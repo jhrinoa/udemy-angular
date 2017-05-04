6b97fc413906819f37f95c0fc03b649a985437c6
Jon Lee <Jon.Lee@globalrelay.net>
2017-05-03 18:57:25 -0700

    Lecture17-Using-Custom-Components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-server',
+    templateUrl: 'server.component.html'
+})
+
+export class ServerComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
